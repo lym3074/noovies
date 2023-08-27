be918503444f6223f2aeaf809464fe1026c8b8d9
Yongmin <qwelllo@naver.com>
2023-08-27 23:47:46 +0900

    [3.2]  TMDB Api url 가져오기

--- Screens/Movies.tsx
+++ Screens/Movies.tsx
@@ -10,24 +10,29 @@ const Container = styled.ScrollView`
 `
 const View = styled.View`
     flex: 1
-`
+`;
+
+const API_KEY = 'ef0d1a8e7f2e44edcc57e73fdd30ad39';
 
 const {height: SCREEN_HEIGHT} = Dimensions.get("screen");
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => (
-    <Container>
-        <Swiper
-            loop
-            timeout={2}
-            controlsEnabled={false}
-            containerStyle={{width: "100%", height: SCREEN_HEIGHT/4}}
-        >
-            <View style={{backgroundColor: "red"}}></View>
-            <View style={{backgroundColor: "blue"}}></View>
-            <View style={{backgroundColor: "red"}}></View>
-            <View style={{backgroundColor: "blue"}}></View>
-        </Swiper>
-    </Container>
-);
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+    const url = fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1`)
+    return (
+        <Container>
+            <Swiper
+                loop
+                timeout={2}
+                controlsEnabled={false}
+                containerStyle={{width: "100%", height: SCREEN_HEIGHT/4}}
+            >
+                <View style={{backgroundColor: "red"}}></View>
+                <View style={{backgroundColor: "blue"}}></View>
+                <View style={{backgroundColor: "red"}}></View>
+                <View style={{backgroundColor: "blue"}}></View>
+            </Swiper>
+        </Container>
+    );
+}
 
 export default Movies;
