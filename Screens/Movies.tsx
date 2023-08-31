d8dd7d42682b61068166a95a591892215c65d346
Yongmin <qwelllo@naver.com>
2023-08-31 21:54:30 +0900

    [3.3] 스와이프 lib 변경 및 포스터 정보 적용

--- Screens/Movies.tsx
+++ Screens/Movies.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native"
-import { ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet, useColorScheme } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Swiper from "react-native-web-swiper";
 import { Dimensions } from "react-native";
 import { makeImgPath } from "../utils";
 import { BlurView } from "expo-blur";
+import Swiper from "react-native-swiper";
 
 const Container = styled.ScrollView`
 `
@@ -19,17 +19,49 @@ const Loader = styled.View`
     align-items: center;
 `;
 
+const Poster = styled.Image`
+    width: 100px;
+    height: 160px;
+    border-radius: 5px;
+`;
+
+const Title = styled.Text`
+    font-size: 16px;
+    font-weight: 600;
+    color: white;
+`;
+
+const OverView = styled.Text`
+    margin-top: 10px;
+    color: rgba(255,255,255,0.6);
+`;
+
+const Votes = styled(OverView)`
+    font-size: 12px;
+`;
+
+const Wrapper = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height : 100%;
+`;
+const Column = styled.View`
+    width: 40%;
+    margin-left: 15px;
+    `;
+
 const API_KEY = 'ef0d1a8e7f2e44edcc57e73fdd30ad39';
 
 const BgImg = styled.Image`
 `;
 
-const Title = styled.Text`
-`
+
 
 const {height: SCREEN_HEIGHT} = Dimensions.get("screen");
 
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+    const isDark = useColorScheme() === "dark"? true: false;
     const [loading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
     const getNowPlaying = async () => {
@@ -54,9 +86,12 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
     :
     <Container>
         <Swiper
+            horizontal
+            showsButtons={false}
+            autoplay
+            autoplayTimeout={3.5}
+            showsPagination={false}
             loop
-            timeout={2}
-            controlsEnabled={false}
             containerStyle={{width: "100%", height: SCREEN_HEIGHT/4}}
         >
             {nowPlaying.map(movie => 
@@ -64,8 +99,16 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
                 <BgImg 
                     style={StyleSheet.absoluteFill}
                     source={{uri: makeImgPath(movie.backdrop_path)}} />
-                <BlurView style={StyleSheet.absoluteFill} intensity={80}>
-                    <Title>{movie.original_title}</Title>
+                <BlurView style={StyleSheet.absoluteFill} intensity={90} tint={isDark?"dark":"light"} >
+                    <Wrapper>
+                        <Poster source={{uri: makeImgPath(movie.poster_path)}}/>
+                        <Column>
+                            <Title>{movie.original_title}</Title>
+                            {movie.vote_average > 0 &&<Votes>⭐️{movie.vote_average}/10</Votes>}
+                            <OverView>{movie.overview.slice(0, 90)}...</OverView>
+                            
+                        </Column>
+                    </Wrapper>
                 </BlurView>
             </View>)}
             
